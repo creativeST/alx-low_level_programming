4907e00705e65e786584b3d0813d9f0b129dedf8
Sara <tamimsara.i@gmail.com>
2023-03-31 19:36:57 -0700

    commit 1

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,11 +14,11 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j=0; j<10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == s1[j])
+			if (s[i] == s1[j])
 			{
-				n[i] = s2[j];
+				s[i] = s2[j];
 			}
 		}
 	}

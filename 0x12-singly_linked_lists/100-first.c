60f5221555f91cedead1a6e61e999d8b2cd56946
Sara <tamimsara.i@gmail.com>
2023-04-29 06:02:16 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ * printbeforemain - print function
+ *
+ */
+void printbeforemain(void)
+{
+    printf(
+            "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+          );
+}

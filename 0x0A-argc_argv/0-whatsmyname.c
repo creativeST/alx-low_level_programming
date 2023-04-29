22185f3a2785e8abe6039ae999f99356e712643c
Sara <tamimsara.i@gmail.com>
2023-04-29 07:13:48 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 
+ */
+int main(int argc, char *argv[])
+{
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
